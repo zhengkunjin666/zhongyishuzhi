7f41c69c655734e0a17397f357e47b45498ec7ca
zhengkunjin <zhengkunjin@126.com>
2022-10-12 16:31:04 +0800

    update img

--- src/global/request/api.js
+++ src/global/request/api.js
@@ -1,4 +1,4 @@
-const API_PREFIX = "https://" + import.meta.env.VITE_APP_DOMAIN;
+const API_PREFIX = import.meta.env.VITE_APP_API;
 export default {
   experts: `${API_PREFIX}/api/admin/experts`,
   projects: `${API_PREFIX}/api/admin/projects`,
